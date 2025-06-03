d903d66a3e0a301d89518876e45ff2c9052cfa31
Elias Adjetey <elias.adjetey@amalitechtraining.org>
2025-06-03 22:02:34 +0000

    feat: set up PostgreSQL, MongoDB, and application with docker

---
+++ src/test/java/com/buildmaster/project_tracker/ProjectTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmaster.project_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
